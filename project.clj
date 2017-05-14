1fe458e13a05d267e773aca322649e7e04a091c1
Alex Robbins <alexdotrobbins@gmail.com>
2017-05-14 16:42:27 -0500

    Add project metadata

--- project.clj
+++ project.clj
@@ -1,4 +1,8 @@
 (defproject ee5393-hw5 "0.1.0-SNAPSHOT"
+  :description "For the synthesis of multi-terminal switching circuits"
+  :url "https://github.com/alex-robbins/ee5393-hw5"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/math.combinatorics "0.1.4"]]
 
